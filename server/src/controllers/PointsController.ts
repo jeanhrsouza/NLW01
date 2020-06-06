eb2d5afc36e477407c0aa478aa1af95a5b7ee18c
Jean Souza <jeanhrsouza@hotmail.com>
2020-06-05 21:43:08 -0300

    NLW - Aula 05

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,15 +1,13 @@
-import { Request, Response } from 'express'
 import knex from '../database/connection';
+import { Request, Response } from 'express';
 
 class PointsController {
-
     async index(request: Request, response: Response) {
-        //cidade, uf, items (Query Params)
         const { city, uf, items } = request.query;
 
         const parsedItems = String(items)
             .split(',')
-            .map(item => Number(item.trim()));
+            .map(item => Number(item.trim()))
 
         const points = await knex('points')
             .join('point_items', 'points.id', '=', 'point_items.point_id')
@@ -17,12 +15,17 @@ class PointsController {
             .where('city', String(city))
             .where('uf', String(uf))
             .distinct()
-            .select('points.*');
+            .select('points.*')
 
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+            };
+        });
 
-        return response.json(points);
+        return response.json(serializedPoints)
     }
-
 
     async show(request: Request, response: Response) {
         const { id } = request.params;
@@ -33,19 +36,17 @@ class PointsController {
             return response.status(400).json({ message: 'Point not found' });
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+        };
 
-        /*
-            SELECT *
-            FROM items
-            JOIN point_items ON items.id = point_items.item_id
-            WHERE point_items.point_id = {id}
-        */
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ serializedPoint, items });
     }
 
     async create(request: Request, response: Response) {
@@ -57,32 +58,35 @@ class PointsController {
             longitude,
             city,
             uf,
-            items
+            items,
         } = request.body;
 
         const trx = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
             latitude,
             longitude,
             city,
-            uf
-        };
+            uf,
+        }
 
         const insertedIds = await trx('points').insert(point);
 
         const point_id = insertedIds[0];
 
-        const pointItems = items.map((item_id: number) => {
-            return {
-                item_id,
-                point_id,
-            }
-        })
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id,
+                }
+            });
 
         await trx('point_items').insert(pointItems);
 
@@ -93,6 +97,6 @@ class PointsController {
             ...point,
         });
     }
-}
+};
 
 export default PointsController;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,19 @@
 import express from 'express';
-import cors from 'cors';
 import path from 'path';
 import routes from './routes';
-
+import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333);
+app.use(errors());
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
